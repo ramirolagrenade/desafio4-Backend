9fd0199412e7ab8a1094d27d89ef92ce4319f5a6
Ramiro Lagrenade <ramirolagrenade@gmail.com>
2023-04-19 18:24:10 -0300

    desafio

---
+++ src/app.js
@@ -0,0 +1,38 @@
+import express from "express"
+import handlebars from "express-handlebars"
+import { Server } from "socket.io"
+
+import __dirname from "./utils.js"
+import viewRouter from './routes/views.routes.js'
+
+const PORT = 8080
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static(__dirname+'/public'))
+
+app.engine('handlebars', handlebars.engine())
+
+app.set('views',__dirname+'/views')
+app.set('view engine','handlebars')
+
+app.use('/', viewRouter)
+// app.use('/realtimeproducts',)
+
+const server = app.listen(PORT, ()=>{
+    console.log('Servidor funcionando en el puerto '+PORT)
+})
+
+const socketServerIO = new Server(server)
+
+socketServerIO.on('connection', socket =>{
+    console.log('usuario conectado')
+
+    socket.on("menssage",data =>{
+        socketServerIO.emit('log',data )
+    })
+})
+
+
+
---
+++ src/routes/views.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express"
+import ProductManager from "../manager/ProductManager.js"
+
+const  router = Router()
+
+const productManager = new ProductManager('./src/file/products.json')
+
+router.get('/',async (req,res) =>{
+
+    let product = await productManager.getProducts()
+
+    console.log(product)
+
+    res.render('home',{products: product})
+})
+
+// router.get('/realtimeproducts', (req,res) =>{
+//     res.render('realtimeproducts')
+// })
+
+export default router
